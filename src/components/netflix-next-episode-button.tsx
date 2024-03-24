1341fdd0ea888a0e3b6f649f5f02b0f8f5e8e872
Juraj Žec <juro.zec.1@gmail.com>
2024-03-24 13:14:12 +0100

    copied second approach with more funky colors. Included labels to describe each approach.

--- src/components/netflix-next-episode-button.tsx
+++ src/components/netflix-next-episode-button.tsx
@@ -46,8 +46,8 @@ const NetflixNextEpisodeButton: FC = () => {
   }
 
   return (
-    <div className="space-y-8">
-      <div className="relative">
+    <div className="max-w-64 space-y-8">
+      <div className="relative w-full">
         <button
           className="absolute -left-4 top-1/2 -translate-x-full -translate-y-1/2"
           onClick={toggleAnimation}
@@ -66,30 +66,62 @@ const NetflixNextEpisodeButton: FC = () => {
           onValueChange={(val) => loading.jump(val[0]!)}
         />
       </div>
-      <button
-        className="relative flex items-center overflow-hidden rounded-md bg-neutral-800 px-6 py-2 text-2xl font-medium text-white"
-        onClick={startAnimation}
-      >
-        <NetflixPlayIcon className="mr-5" />
-        Next episode
-        <motion.div
-          style={{ x: positionX }}
-          className="pointer-events-none absolute inset-0 h-full w-full -translate-x-full backdrop-invert"
-        />
-      </button>
-      <motion.button
-        className="relative flex items-center overflow-hidden rounded-md bg-gradient-to-r from-white from-50% to-neutral-500 to-50% bg-[length:200%_200%] text-2xl font-medium text-white"
-        onClick={startAnimation}
-        style={{ backgroundPositionX }}
-      >
-        <motion.div
-          className="to-grey h-full w-full space-x-5 bg-gradient-to-r from-black from-50% to-white to-50% bg-[length:200%_200%] bg-clip-text px-6 py-2 font-semibold text-transparent"
+      <div className="flex flex-col items-start gap-2">
+        <button
+          className="relative flex items-center justify-center overflow-hidden rounded-md bg-neutral-800 px-6 py-2 text-2xl font-medium text-white"
+          onClick={startAnimation}
+        >
+          <NetflixPlayIcon className="mr-5" />
+          Next episode
+          <motion.div
+            style={{ x: positionX }}
+            className="pointer-events-none absolute inset-0 h-full w-full -translate-x-full backdrop-invert"
+          />
+        </button>
+        <p className="text-xs">
+          1. overlay with <code>backdrop-filter: invert(100%);</code>. Does work
+          on SVG too but has limited color capabilities.
+        </p>
+      </div>
+      <div className="flex flex-col items-start gap-2">
+        <motion.button
+          className="relative flex items-center justify-center overflow-hidden rounded-md bg-gradient-to-r from-white from-50% to-neutral-500 to-50% bg-[length:200%_200%] text-2xl"
+          onClick={startAnimation}
           style={{ backgroundPositionX }}
         >
-          <span className="mr-5">▶</span>
-          Next episode
-        </motion.div>
-      </motion.button>
+          <motion.div
+            className="h-full w-full space-x-5 bg-gradient-to-r from-black from-50% to-white to-50% bg-[length:200%_200%] bg-clip-text px-6 py-2 font-bold text-transparent"
+            style={{ backgroundPositionX }}
+          >
+            <span className="mr-5">▶</span>
+            Next episode
+          </motion.div>
+        </motion.button>
+        <p className="text-xs">
+          2a. Two gradients of twice the length of the button, with sharp color
+          edge in the middle. One for text with{' '}
+          <code>background-clip: text;</code> and one for background.
+        </p>
+      </div>
+      <div className="flex flex-col items-start gap-2">
+        <motion.button
+          className="relative flex items-center justify-center overflow-hidden rounded-md bg-gradient-to-r from-blue-600 from-50% to-neutral-500 to-50% bg-[length:200%_200%] text-2xl"
+          onClick={startAnimation}
+          style={{ backgroundPositionX }}
+        >
+          <motion.div
+            className="h-full w-full space-x-5 bg-gradient-to-r from-orange-600 from-50% to-white to-50% bg-[length:200%_200%] bg-clip-text px-6 py-2 font-bold text-transparent"
+            style={{ backgroundPositionX }}
+          >
+            <span className="mr-5">▶</span>
+            Next episode
+          </motion.div>
+        </motion.button>
+        <p className="text-xs">
+          2b. Allows for better control over the color, but doesn't support
+          icons. The leading "play" icon is actually a Unicode character.
+        </p>
+      </div>
     </div>
   )
 }
